0c338ba48ee83b61a505598d1145c70ce724b9d7
Xiong <wooiseongseong96@gmail.com>
2024-12-15 17:23:06 +0800

    ver5

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),    
     output: {        
         path: path.resolve(__dirname, 'dist'),        
-        filename: 'index.js',        
+        filename: '/index.js',        
         clean: true
     },
     module: {
