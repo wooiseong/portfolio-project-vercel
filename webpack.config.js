4b5da4facce48a3c04144ff2afb5d130413b2d9c
Xiong <wooiseongseong96@gmail.com>
2024-12-15 17:45:44 +0800

    ver9

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     },
     plugins: [
       new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './src/index.html'
     }),
       new MiniCssExtractPlugin(),
       new webpack.DefinePlugin({
